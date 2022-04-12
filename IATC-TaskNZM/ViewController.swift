4ba2f7b39a1728a49ef132c1196dfe439ccc49db
Nazim Asadov <nazimatmu@gmail.com>
2022-04-13 00:56:05 +0400

    Initial Commit

--- IATC-TaskNZM/ViewController.swift
+++ IATC-TaskNZM/ViewController.swift
@@ -11,8 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+        setupImg()
+        view.backgroundColor = .gray
     }
+    
+    func setupUI() {
+        let myLabel = UILabel()
+        myLabel.text = "IATC"
+        myLabel.textColor = .white
+//        myLabel.font.pointSize = 18.0
+        myLabel.frame = CGRect(x: 175, y: 150, width: 100, height: 40)
+        view.addSubview(myLabel)
+    }
+    
+    func setupImg() {
+        let myImage = UIImageView(frame: CGRect(x: 100, y: 300, width: 200, height: 200))
+        myImage.image = UIImage(named: "iatc")
+        view.addSubview(myImage)
+    }
+    
+    
 
 
 }
